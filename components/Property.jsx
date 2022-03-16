51111323fc4031d29366c818d86d9d8dfbd9361e
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2022-03-16 21:09:34 +0000

    Retrieving more houses from API

--- components/Property.jsx
+++ components/Property.jsx
@@ -6,6 +6,8 @@ import { BsGridFill } from 'react-icons/bs'
 import { GoVerified } from 'react-icons/go'
 import millify from 'millify'
 
+import DefaultImage from '../assets/house.webp'
+
 const Property = ({
   property: {
     coverPhoto,
@@ -21,7 +23,36 @@ const Property = ({
   },
 }) => (
   <Link href={`/property/${externalID}`} passHref>
-    {title}
+    <Flex
+      flexWrap="wrap"
+      w="420px"
+      p="5"
+      paddingTop="0"
+      justifyContent="flex-start"
+      cursor="pointer">
+      <Box>
+        <Image
+          src={coverPhoto ? coverPhoto.url : DefaultImage}
+          width={400}
+          height={260}
+          alt="house"
+        />
+      </Box>
+      <Box w="full">
+        <Flex paddingTop="2" alignItems="center" justifyContent="space-between">
+          <Flex alignItems="center">
+            <Box paddingRight="3" color="green.400">
+              {isVerified && <GoVerified />}
+            </Box>
+            <Text fontWeight="bold" fontSize="lg">
+              AED
+              {millify(price)}
+              {rentFrequency && `/${rentFrequency}`}
+            </Text>
+          </Flex>
+        </Flex>
+      </Box>
+    </Flex>
   </Link>
 )
 export default Property
